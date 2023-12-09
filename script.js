11e21608c89ddd1076c9a06b3e116b0ef9c8e222
Ash <ashloraptor@gmail.com>
2023-12-09 12:36:00 -0500

    Included Next button and Submit button.

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 //query select button
 var startButton = document.querySelector(".start-button");
+var nextButton = document.querySelector(".next-button");
+var submitButton = document.querySelector(".submit-button");
 var timerElement = document.querySelector(".timer-count");
 var container = document.querySelector(".container");
 //var questions = document.querySelector(".questions");
@@ -36,6 +38,8 @@ var timerCount;
 //init function
 function init() {
   container.hidden = true
+  nextButton.hidden = true
+  submitButton.hidden = true
 }
 
 
@@ -46,7 +50,9 @@ function startQuiz() {
   // Hides start button when quiz is in progress
   startButton.hidden = true;
   startTimer();
+  //Shows question and Next button
   container.hidden = false
+  nextButton.hidden = false
   renderQuestion();
 
 }
@@ -68,12 +74,11 @@ function startTimer() {
 
 //Presents the questions
 for (var i = 0; i < questions.length; i++) {
-
 }
 
 function renderQuestion() {
   // Randomly picks question from questions array
-  chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
+  //chosenQuestion = questions[Math.floor(Math.random() * questions.length)];
   //Shows on screen
 
   var output = [];
@@ -98,9 +103,9 @@ function renderQuestion() {
   container.innerHTML = output.join('')
 }
 
-renderQuestion();
 
 
+//create a variable to track the current question you're on; so when a person clicks/triggers the  nextQuestion function. it would increment the currentQuestionIndex first, then trigger the renderQuestion
 
 //Checks if correct
 function checkCorrect() {
@@ -117,3 +122,6 @@ function checkCorrect() {
 init();
 // Attach event listener to start button to call startGame function on click
 startButton.addEventListener("click", startQuiz);
+
+//nextButton.addEventListener("click", submit);
+//submitButton.addEventListener("click", endQuiz);
